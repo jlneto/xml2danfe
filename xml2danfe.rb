4100fe31f66024462084d5126df9eba6c9b4e955
joselopesneto <jose.lopes.santos.neto@gmail.com>
2016-08-25 15:27:47 -0300

    Adicionada a Documentação na execução

--- xml2danfe.rb
+++ xml2danfe.rb
@@ -1,7 +1,14 @@
 require "ruby_danfe"
+
+puts 'Procurando *.xml no diretorio corrente e todos os subdiretórios...'
 xmls = Dir.glob("**/*.xml")
+n = 0
+puts 'Iniciando conversão'
 xmls.each do |xml|
   dest = xml.gsub('.xml','.pdf')
   RubyDanfe.generate(dest, xml)
   print '.'
+  n += 1
 end
+puts ''
+puts "Gerado(s) #{n} PDF(s)!"
